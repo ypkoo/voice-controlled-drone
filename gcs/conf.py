6525a9b8e987a4793c5efb84a78c4e0b1e0848c8
ypkoo <ypkoo93@gmail.com>
2016-07-31 18:41:39 +0900

    add thing detail png

--- gcs/conf.py
+++ gcs/conf.py
@@ -1,7 +1,7 @@
 cert_path = "awsCerts/"
 host = "your-host.iot.us-east-1.amazonaws.com"
 port = 8883
-topic = "aws/things/your-topic-name/"
+topic = "aws/things/your-thing-name/"
 root_cert = cert_path + "root-CA.crt"
 cert_file = cert_path + "your-certificate.pem.crt"
 key_file = cert_path + "your-private.pem.key"
